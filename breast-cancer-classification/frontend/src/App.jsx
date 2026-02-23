ecba1bc702dc4793211f3fd0fb9638a1a6ffaf76
Bahri Keskin <keskinbhri@gmail.com>
2026-02-23 03:01:31 +0300

    Frontend API linki Render sunucusu ile güncellendi

--- breast-cancer-classification/frontend/src/App.jsx
+++ breast-cancer-classification/frontend/src/App.jsx
@@ -64,7 +64,7 @@ function App() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict/", {
+      const response = await fetch("https://breast-cancer-classification-b5ix.onrender.com/predict/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
